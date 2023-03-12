926628dca0fd7e6f66a507be6ef2c2e596beb1fc
nguyenanvi122333 <nguuyenanvi122333@gmail.com>
2023-03-12 15:44:38 +0700

    Fix localization when change on some pages

--- resources/views/components/localization/alang.blade.php
+++ resources/views/components/localization/alang.blade.php
@@ -1,7 +1,7 @@
   @foreach($available_locales as $locale_name => $available_locale)
     <button  uk-tooltip="{{$locale_name}}" type="button"
       class="uk-button-secondary" style="padding: 2px; "
-            onclick="window.location.href='language/{{ $available_locale }}'">
+            onclick="window.location.href='{{ route('admin.home') }}/language/{{ $available_locale }}'">
       <img src="{{asset('storage/images/logo/lang/'.$available_locale.'.png')}}" height="20" width="20 ">
     </button>
   @endforeach
